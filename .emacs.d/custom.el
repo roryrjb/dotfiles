8923adb85ecd1c6c43b4cede20880c44b7b6c345
Rory Bradford <roryrjb@gmail.com>
2025-08-24 13:59:09 +0100

    Initial commit

---
+++ .emacs.d/custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(doric-water))
+ '(inhibit-startup-screen t)
+ '(package-selected-packages
+	 '(doric-themes smex ido-completing-read+ dtrt-indent magit markdown-mode multi-vterm typescript-mode vterm vue-mode yaml-mode))
+ '(warning-suppress-log-types '((comp))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Hack" :foundry "SRC" :slant normal :weight regular :height 128 :width normal)))))
---
+++ .emacs.d/init.el
@@ -0,0 +1,74 @@
+(setq custom-file "~/.emacs.d/custom.el")
+
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+(global-display-line-numbers-mode)
+
+(setq project-switch-commands 'project-dired)
+(setq compile-command "")
+(setq ring-bell-function 'ignore)
+(setq default-directory-tracking nil)
+(setq make-backup-files nil
+      auto-save-default nil
+      create-lockfiles nil)
+(setq magit-no-confirm '(discard delete stage-all-changes))
+(setq case-fold-search nil)
+(setq-default tab-width 2)
+;; (setq-default electric-indent-inhibit t)
+
+(savehist-mode 1)
+(save-place-mode 1)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(setq use-package-always-ensure t)
+(package-initialize)
+
+(ido-ubiquitous-mode 1)
+(ido-mode 1)
+(ido-everywhere 1)
+
+(add-hook 'mmm-mode-hook
+          (lambda ()
+            (set-face-background 'mmm-default-submode-face nil)))
+
+(add-hook 'find-file-hook
+          (lambda ()
+            (when-let ((project (project-current)))
+              (setq default-directory (project-root project)))))
+
+(global-set-key (kbd "C-:") 'goto-line)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "C-<return>") 'compile)
+(global-set-key (kbd "C-<end>") 'kill-compilation-hard)
+(global-unset-key (kbd "C-z")) ; suspend
+(global-set-key (kbd "C-S-d") 'duplicate-line)
+(global-set-key (kbd "C-p") 'dabbrev-expand) ; overrides C-p (previous-line)
+(global-set-key (kbd "C-x f") 'find-file)
+(global-set-key (kbd "C-x C-b") 'switch-to-buffer)
+(global-set-key (kbd "C-c t") 'multi-vterm)
+(global-set-key (kbd "C-S-k") 'kill-whole-line)
+(global-set-key (kbd "C-x C-k") 'kill-buffer)
+(global-set-key (kbd "C-x C-d") 'dired)
+
+(add-hook 'compilation-filter-hook 'ansi-color-compilation-filter)
+
+(add-to-list 'default-frame-alist '(height . 50))
+
+(eval-after-load "dired" '(progn
+  (define-key dired-mode-map [mouse-2] 'dired-mouse-find-file) ))
+
+(load custom-file)
+
+(defun kill-compilation-hard ()
+  "Kill compilation process with SIGKILL from any buffer."
+  (interactive)
+  (let ((comp-buf (get-buffer "*compilation*")))
+    (if comp-buf
+        (with-current-buffer comp-buf
+          (when (get-buffer-process comp-buf)
+            (let ((proc (get-buffer-process comp-buf)))
+              (signal-process proc 'SIGKILL)
+              (message "Compilation process killed with SIGKILL"))))
+      (message "No compilation buffer found"))))
